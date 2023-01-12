c0ff95f4bee48910fe6e501954db40b6ba600daf
Renato Martins <rmartins6694@gmail.com>
2023-01-12 18:44:37 -0300

    add new Components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import {  PostCard, Categoires, PostWidget, Categories } from '../components'
 
 
 const posts = [
@@ -8,20 +9,23 @@ const posts = [
 
 export default function Home () {
   return (
-    <div className="mx-aut px-10 mb-8">
+    <div className="mx-auto px-10 mb-8">
       <Head>
         <title> My Blog</title>
         <link ref='icon' href="/favicon.ico"/>
       </Head>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
-
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) =>  <PostCard post={post} key={post.title}/>)}
+        </div>
+        
+        <div className="lg:col-sapn-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
       </div>
     </div>
   )
