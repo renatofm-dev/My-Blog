6e9efce642d22bb6a391c5d3070e744d4fd00f08
Renato Martins <rmartins6694@gmail.com>
2023-01-14 13:40:18 -0300

    change background and text-color

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
                 <Link href="/">
-                    <span className='cursor-pointer font-bold text-4xl text-white'>
+                    <span className='cursor-pointer font-bold text-4xl text-black'>
                         My Blog
                     </span>
                 </Link>
@@ -18,7 +18,7 @@ const Header = () => {
             <div className='hidden md:float-left md:contents'>
                 {categories.map((category, index) => (
                     <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                        <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
                             {category.name}
                         </span>
                     </Link>
