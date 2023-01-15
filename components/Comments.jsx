54bd703be08bcefe00aa1d99a85f9f9ecf8e9f2b
Renato Martins <rmartins6694@gmail.com>
2023-01-15 11:53:58 -0300

    create Comments.jsx

---
+++ components/Comments.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Comments = () => {
+  return (
+    <div>
+        <h3>
+            Comments
+        </h3>
+    </div>
+  )
+}
+
+export default Comments
--- components/index.js
+++ components/index.js
@@ -6,4 +6,6 @@ export {  default as Layout } from './Layout';
 export {  default as PostDetail } from './PostDetail';
 export {  default as Author } from './Author';
 export {  default as CommentsForm } from './CommentsForm';
+export {  default as Comments } from './Comments';
+
 
