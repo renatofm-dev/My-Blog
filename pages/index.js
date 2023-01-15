4b39d3de982e8795f0f3b089183895c1c1d2a4f4
Renato Martins <rmartins6694@gmail.com>
2023-01-15 11:03:03 -0300

    adjustments in PostWidget.jsx

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,18 @@ import { getPosts } from '../services';
 
 export default function Home ({  posts }) {
   return (
-    <div className="mx-auto px-10 mb-8">
+    <div className=" container mx-auto px-10 mb-8">
       <Head>
         <title> My Blog</title>
         <link ref='icon' href="/favicon.ico"/>
       </Head>
 
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+      <div className=" grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) =>  <PostCard post={post.node} key={post.title}/>)}
         </div>
         
-        <div className="lg:col-sapn-4 col-span-1">
+        <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
               <PostWidget />
               <Categories />
--- services/index.js
+++ services/index.js
@@ -38,3 +38,47 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    GetPostDetails($slug: String!, $categories: [String!] ){
+      posts(
+        where: { slug_not: $slug, AND: { categories_some: { sulg_in: $categories}}}
+        las: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
